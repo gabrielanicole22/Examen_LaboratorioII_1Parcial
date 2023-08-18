7cd23bd22d1886ddbe02bc23aa59b29625189cd9
HP <HP@PaviTallulah>
2023-08-18 13:37:45 -0600

    Creacion clase comment

---
+++ src/examen_labprograii_iparcial/Comment.java
@@ -0,0 +1,35 @@
+package examen_labprograii_iparcial;
+
+import java.util.Calendar;
+
+/**
+ *
+ * David Zelaya
+ */
+
+public class Comment {
+
+private int postID;
+private String Autor;
+private String comentario;
+private Calendar Fecha;
+
+public Comment (int id,String autor, String comment){
+    this.postID=id;
+    this.Autor=autor;
+    this.comentario=comment;
+    Fecha=Calendar.getInstance();
+}
+
+public String list(){
+    String listar;
+    
+    listar="Autor: "+Autor+"- Fecha: "+Fecha
+        +"\n "+comentario;
+    
+    return listar;
+}
+
+
+    
+}
